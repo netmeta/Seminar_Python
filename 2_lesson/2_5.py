1efed1be75e56b1e0f0d9750056327e8ae4f00fd
netmeta <moron686@gmail.com>
2023-02-27 02:31:07 +0300

    homework

--- 2_lesson/2_5.py
+++ 2_lesson/2_5.py
@@ -1,2 +1,11 @@
 for i in range(1, 21, 2):
     print(i, end=" ")  # в строку
+
+n = ["a", "d", "f", "y", "u"]
+
+for i in range(len(n)):
+    n[i] *= 5
+print(n)
+
+for i, v in enumerate(n, 1):  # список
+    print(f"{i}) {v}")
