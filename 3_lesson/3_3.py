a1fa9f3ba78863b1de5c123c56e655dfaf4fe6bb
netmeta <moron686@gmail.com>
2023-03-08 18:43:31 +0300

    add all task

--- 3_lesson/3_3.py
+++ 3_lesson/3_3.py
@@ -4,3 +4,11 @@
 # предыдущего (элемента с предыдущим номером)
 list_nums = [0, -1, 5, 2, 3]
 print(sum([list_nums[i] > list_nums[i-1] for i in range(0, len(list_nums))]))
+
+m = [0, -1, 5, 2, 3]
+count = 0
+a = 0
+for i in range(0, len(m) - 1, 2):
+    if i + 1 > i:
+        count += 1
+print(count)
