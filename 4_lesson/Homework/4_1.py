2f88be88c04476d33e4b409cefeb750badd2392c
netmeta <moron686@gmail.com>
2023-03-08 19:25:47 +0300

    add hw

--- 4_lesson/Homework/4_1.py
+++ 4_lesson/Homework/4_1.py
@@ -5,3 +5,27 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+
+mol = [int(x) for x in input().split()]
+n = mol[0]
+m = mol[1]
+set_1 = set()
+set_2 = set()
+list_1 = list()
+
+a = [int(x) for x in input().split()]
+k = set(a)
+for i in k:
+    set_1.add(i)
+
+b = [int(x) for x in input().split()]
+k1 = set(b)
+
+for i in k1:
+    set_2.add(i)
+
+lok = set_1 & set_2
+kool = list(lok)
+kool.sort()
+for i in kool:
+    print(i, end=' ')
--- 4_lesson/Homework/4_2.py
+++ 4_lesson/Homework/4_2.py
@@ -4,3 +4,14 @@
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
 # 4 -> 1 2 3 4
 # 9
+n = int(input())
+arr = list()
+for i in range(n):
+    x = int(input())
+    arr.append(x)
+
+arr_count = list()
+for i in range(len(arr)-1):
+    arr_count.append(arr[i-1] + arr[i] + arr[i+1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
