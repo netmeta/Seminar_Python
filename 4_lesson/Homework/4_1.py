f7a1e40c3ab087ebf903baca8ad071f52d8cca71
netmeta <moron686@gmail.com>
2023-03-08 19:37:35 +0300

    add task

--- 4_lesson/Homework/4_1.py
+++ 4_lesson/Homework/4_1.py
@@ -6,33 +6,33 @@
 # 3 6 9 12 15 18
 # 6 12
 
-mol = [int(x) for x in input().split()]
-n = mol[0]
-m = mol[1]
-set_1 = set()
-set_2 = set()
-list_1 = list()
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
 
-a = [int(x) for x in input().split()]
-k = set(a)
-for i in k:
-    set_1.add(i)
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
 
-b = [int(x) for x in input().split()]
-k1 = set(b)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
 
-for i in k1:
-    set_2.add(i)
+# for i in k1:
+#     set_2.add(i)
 
-lok = set_1 & set_2
-kool = list(lok)
-kool.sort()
-for i in kool:
-    print(i, end=' ')
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end=' ')
 
 
 # solution 2
-n, m = input().split()
+n1, m1 = input().split()
 first = [int(i) for i in input().split()]
 second = [int(j) for j in input().split()]
 
